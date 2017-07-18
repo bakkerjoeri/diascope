bb799f1fe509b8d2a04b94b21e6c036736aaf910
Joeri Bakker <bakkerjoeri@gmail.com>
2017-07-18 18:22:13 +0200

    Updated demo

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -3,7 +3,8 @@ let Diascope = window.Diascope;
 let frame = document.querySelector('.js-diascope-frame');
 let reel = document.querySelector('.js-diascope-reel');
 let options = {
-	step: 1,
+	step: 3,
+	shouldCenter: true
 };
 
 new Diascope(frame, reel, options);
