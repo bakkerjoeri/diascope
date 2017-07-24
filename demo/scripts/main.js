9d6a82f5b78ab32865b761e94ca1575efe942d67
Joeri Bakker <bakkerjoeri@gmail.com>
2017-07-24 12:19:01 +0200

    Added navigate elements in options

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -1,10 +1,15 @@
-let Diascope = window.Diascope;
+const Diascope = window.Diascope;
 
-let frame = document.querySelector('.js-diascope-frame');
-let reel = document.querySelector('.js-diascope-reel');
-let options = {
-	step: 3,
-	shouldCenter: true
+let frame, reel, navigateNext, navigatePrevious, options;
+
+frame = document.querySelector('.js-diascope-frame');
+reel = document.querySelector('.js-diascope-reel');
+navigateNext = document.querySelector('.js-diascope-navigate-next');
+navigatePrevious = document.querySelector('.js-diascope-navigate-previous');
+
+options = {
+	elementNavigateNext: navigateNext,
+	elementNavigatePrevious: navigatePrevious,
 };
 
 new Diascope(frame, reel, options);
